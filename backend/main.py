c813b857c9af8b40bb9c158f05dd6e1821eabef6
nic5694 <nicholasmartoccia04@icloud.com>
2025-02-01 18:12:33 -0500

    added healthcheck

--- backend/main.py
+++ backend/main.py
@@ -57,6 +57,9 @@ async def detect_objects(file: UploadFile = File(...)):
 
     return detections
 
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     import uvicorn

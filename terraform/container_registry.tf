78ebf6ddf231e2743e3e9d3f0bb7f2dbac25493b
nic5694 <nicholasmartoccia04@icloud.com>
2025-02-01 15:47:40 -0500

    fixed the pipeline and added updated terraform infrastructure

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -3,6 +3,5 @@ resource "azurerm_container_registry" "azure_container_registry" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
+  admin_enabled       = true
 }
-
-// to login to the registry it is as simple as docker login myregistry.azurecr.io, then you can tag and push the image to the registry

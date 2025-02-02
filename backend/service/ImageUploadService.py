c5dab3a27069097c7309491211b716a20b36d767
William Dunwoody <williamdunwoody1@gmail.com>
2025-02-02 05:36:30 -0500

    idea has been had

--- backend/service/ImageUploadService.py
+++ backend/service/ImageUploadService.py
@@ -13,6 +13,7 @@ class ImageUploadService:
 
     async def upload_image(self, image: UploadFile) -> str:
         filename = image.filename
+        print(filename)
         file_extension = filename.rsplit('.', 1)[1]
         random_name = id_generator() + '.' + file_extension
         blob_client = self.container_blob.get_blob_client(random_name)

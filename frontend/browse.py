2d7b934f4a246457d39198a718f4051066cc23ec
William Dunwoody <williamdunwoody1@gmail.com>
2025-02-01 21:21:31 -0500

    get all lost items no longer has the embedded field of the image object attached

--- frontend/browse.py
+++ frontend/browse.py
@@ -1,5 +1,34 @@
 import streamlit as st
+import requests
 
+BACKEND_URL = (
+    "https://404lostandfound-aserdfh2csb8cmfh.canadacentral-01.azurewebsites.net"
+)
 
 def main():
+
+    lost_items = requests.get(BACKEND_URL + "/lostitem/getAll")
+
     st.write("Browse Page")
+    st.write("Here are the lost items:")
+
+    lost_items = lost_items.json()["items"]
+
+    print(len(lost_items))
+
+    # st.write(lost_items)
+    st.write(len(lost_items))
+
+    # for item in lost_items:
+    #     st.write("a")
+
+    # for item in lost_items:
+        # st.write(item)
+        # card(item["image_url"], item["description"], item["time_found"])
+
+
+def card(image_url, description, time_found):
+    st.write("card")
+    # st.write(f"Time Found: {time_found}")
+    # st.write(f"Description: {description}")
+    # st.image(image_url, width=400)
